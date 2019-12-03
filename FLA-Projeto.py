872961d251f96023c59eb90219de8ee6fc6b3530
Luís Eduardo da Silva Ribeiro <54729517+edurs2602@users.noreply.github.com>
2019-12-02 21:24:39 -0300

    Add files via upload

--- FLA-Projeto.py
+++ FLA-Projeto.py
@@ -1,28 +1,29 @@
 lista_cadastro = []
 
 def Menu():
-     print('Qual opção deseja?')
-     print('1 - Cadastrar pessoa.')
-     print('2 - Remover pessoa do cadastro.')
-     print('3 - Listar cadastro.')
-     print('4 - Sair do programa.')
-     ca = input()
-     if ca == '1' or ca == '2' or ca == '3' or ca == '4':
-         return int(ca)
-     else:
-         print('Opção inválida!')
-     return -1
+    print('*' * 34)
+    print('* 1 - Cadastrar pessoa           *')
+    print('* 2 - Remover pessoa do cadastro *')
+    print('* 3 - Listar cadastro            *')
+    print('* 4 - Sair do programa           *')
+    print('*' * 34)
+    op = input('Qual opção deseja? ')
+    if op == '1' or op == '2' or op == '3' or op == '4':
+        return int(op)
+    else:
+        print('Opção inválida!')
+    return -1
 
 def fazcadastro():
-     lista1 = []
-     nome = input('Qual é o seu nome?')
-     idade = int(input('Informe sua idade:'))
-     sexo = input('Qual o seu sexo?')
-     lista1.append(nome)
-     lista1.append(idade)
-     lista1.append(sexo)
-     lista_cadastro.append(lista1)
-     print('')
+    lista1 = []
+    nome = input('Qual é o seu nome? ')
+    idade = int(input('Informe sua idade: '))
+    sexo = input('Qual o seu sexo? ')
+    lista1.append(nome)
+    lista1.append(idade)
+    lista1.append(sexo)
+    lista_cadastro.append(lista1)
+    print('')
 
 def listcadastro():
 
@@ -33,23 +34,30 @@ def listcadastro():
 
 def remcadastro():
 
-     nome_procurar = input('Nome da pessoa deseja remover: ')
-     idade_procurar = int(input('Idade da pessoa que você dejesa remover: '))
-     sexo_procurar = input('Sexo da pessoa que você deseja remover: ')
+    nome_procurar = input('Nome da pessoa deseja remover: ')
+    idade_procurar = int(input('Idade da pessoa que você dejesa remover: '))
+    sexo_procurar = input('Sexo da pessoa que você deseja remover: ')
 
-     list_delete = [nome_procurar, idade_procurar, sexo_procurar]
+    list_delete = [nome_procurar, idade_procurar, sexo_procurar]
 
-     lista_cadastro.remove(list_delete)
+    if list_delete in lista_cadastro:
+        print('')
+        print('Removido com sucesso!')
+        return lista_cadastro.remove(list_delete)
+    else:
+        print('')
+        print('Algo foi escrito errado! ')
+    return -1
 
-     print('')
+    print('')
 
 while True:
-    ca = Menu()
-    if ca == 1:
+    op = Menu()
+    if op == 1:
         fazcadastro()
-    elif ca == 2:
+    elif op == 2:
         remcadastro()
-    elif ca == 3:
+    elif op == 3:
         listcadastro()
-    elif ca == 4:
+    elif op == 4:
         break
